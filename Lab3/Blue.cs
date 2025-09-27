179561353c787b01005d36e191a313f41d4b3297
CuttiePie <144522961+OtterCuttie@users.noreply.github.com>
2025-09-27 20:52:32 +0300

    сделала всю синюю лигу

--- Lab3/Blue.cs
+++ Lab3/Blue.cs
@@ -1,4 +1,7 @@
-﻿namespace Lab3
+using System.Diagnostics.Metrics;
+using System.Security.Cryptography;
+
+namespace Lab3
 {
     public class Blue
     {
@@ -7,6 +10,20 @@
             double milk = 0;
 
             // code here
+            int nenorma = 0;
+            double kg = 0;
+            for (int i=0; i<n; i++)
+            {
+                kg=(double.Parse(Console.ReadLine()));
+                if (kg < norma)
+                {
+                    nenorma++;
+                }
+            }
+
+            milk = ((double)(nenorma  * glass) / 1000);
+            Console.WriteLine(nenorma);
+            Console.WriteLine(milk);
 
             // end
 
@@ -17,7 +34,28 @@
             int first = 0, second = 0, third = 0, fourth = 0;
 
             // code here
+            for (int i = 0; i < n; i++)
+            {
+                double x = (double.Parse(Console.ReadLine()));
+                double y = (double.Parse(Console.ReadLine()));
+                if ((x>0) && (y > 0))
+                {
+                    first++;
+                }
+                else if((x < 0) && (y > 0))
+                {
+                    second++;
+                }
+                else if((x < 0) && (y < 0))
+                {
+                    third++;
+                }
+                else if ((x > 0) && (y < 0))
+                {
+                    fourth++;
+                }
 
+            }
             // end
 
             return (first, second, third, fourth);
@@ -25,9 +63,25 @@
         public int Task3(int n)
         {
             int count = 0;
-
+            int g1;
+            int g2;
+            int g3;
+            int g4;
             // code here
-
+            for (int i=0; i < n; i++)
+            {
+                g1 = int.Parse(Console.ReadLine());
+                g2 = int.Parse(Console.ReadLine());
+                g3 = int.Parse(Console.ReadLine());
+                g4 = int.Parse(Console.ReadLine());
+                if ((g1 != 2) && (g2 != 2) && (g3 != 2) && (g4 != 2))
+                {
+                    if ((g1 != 3) && (g2 != 3) && (g3 != 3) && (g4 != 3))
+                    {
+                        count++;
+                    }
+                }
+            }
             // end
 
             return count;
@@ -37,7 +91,25 @@
             int serias = 0;
 
             // code here
+            int seriasTime;
+            int taskTime = 10;
+            serias = 0;
+           while (time < 1440)
+            {
+                if (tasks > 0)
+                {
+                    time += taskTime;
+                    taskTime += 5;
+                    tasks--;
 
+                }
+                else
+                {
+                    seriasTime = int.Parse(Console.ReadLine());
+                    time += seriasTime;
+                    serias++;
+                }
+            }
             // end
 
             return (tasks, serias);
@@ -46,6 +118,31 @@
         {
 
             // code here
+            switch (number)
+            {
+                case 1:
+                    power += 10;
+                    intellect = Math.Max(0, intellect - 5);
+                    break;
+                case 2:
+                    agility += 5;
+                    power = Math.Max(0, power - 5);
+                    intellect = Math.Max(0, intellect - 5);
+                    break;
+                case 3:
+                    power += 10;
+                    intellect = Math.Max(0, intellect - 5);
+                    break;
+                case 4:
+                    agility += 15;
+                    power = Math.Max(0, power - 10);
+                    intellect = Math.Max(0, intellect - 10);
+                    break;
+                case 5:
+                    intellect += 7;
+                    power = Math.Max(0, power - 5);
+                    break;
+            }
 
             // end
 
